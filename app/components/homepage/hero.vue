224fb04b0674a228b5ccb2efbbef4b5f398e8b7e
Ross Hazelett <ross.hazelett@bhspecialty.com>
2025-08-18 23:57:31 -0400

    assets adding

--- app/components/homepage/hero.vue
+++ app/components/homepage/hero.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="relative w-full h-full ">
       <img src="/images/hero.jpg" alt="My Photo" 
-      class=" w-full h-full object-cover z-0"
+      class=" w-full h-full object-cover z-0 opacity-70"
       />
       <div class="absolute inset-20 flex flex-col items-center justify-start z-10">
       <!-- Your overlay content here -->
-      <h1 class="text-white text-4xl font-bold">Kush Mody</h1>
-          <NuxtLink to="/music" class="text-white mt-4">EAR</NuxtLink>
-          <NuxtLink to="/visual" class="text-white mt-4">EYE</NuxtLink>
+      <!-- <h1 class="text-white text-8xl font-bold">Kush Mody</h1> -->
+      <img src="/images/homepage/homepage_header_full.png" alt="Kush Mody Header" class="w-1/2  mt-4" />
+          <!-- <NuxtLink to="/music" class="text-white mt-4">EAR</NuxtLink>
+          <NuxtLink to="/visual" class="text-white mt-4">EYE</NuxtLink> -->
 
 
-      <p class="text-white mt-4">PRODUCER  MIXER  COMPOSER  VISUAL ARTIST</p>
+      <p class="text-black font-bold mt-2">PRODUCER &middot MIXER &middot COMPOSER &middot VISUAL ARTIST</p>
     </div>
 
   </div>
