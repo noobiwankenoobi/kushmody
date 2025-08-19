058dea5c16bbddcdab2994ed3a2ea8d67a6b75af
Ross Hazelett <ross.hazelett@bhspecialty.com>
2025-08-19 10:56:45 -0400

    modal tweaks

--- app/components/media/audio-player-modal.vue
+++ app/components/media/audio-player-modal.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="fixed inset-0 bg-gray bg-opacity-70 flex items-center justify-center z-50 w-screen overflow-y-auto">
-    <div class="bg-white rounded-lg p-6 shadow-lg relative">
+  <!-- Modal All -->
+  <div class="fixed inset-0 flex items-center justify-center z-50 w-screen overflow-y-auto">
+    <!-- Overlay -->
+    <div
+      class="absolute inset-0 bg-gray-500/70 cursor-pointer"
+      @click="$emit('close')"
+    />
+    <!-- Modal Content -->
+    <div class="bg-white p-6 shadow-sm relative">
       <button
         class="absolute top-2 right-2 text-gray-500 hover:text-gray-700 cursor-pointer"
         @click="$emit('close')">✕
